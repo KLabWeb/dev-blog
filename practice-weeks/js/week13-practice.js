e43e282d80da730325561c95682c7de8c59ffd48
CryptoLain <klabweb@tuta.io>
2019-01-09 22:19:03 -0600

    Week 13, Day 02 & 03 added. Sitewide spellcheck & correction.

--- practice-weeks/js/week13-practice.js
+++ practice-weeks/js/week13-practice.js
@@ -28,3 +28,47 @@ console.log(nameSign(chris));
 
 const fs = require('fs');
 const lorem = fs.watch('lorem-ipsum.txt', () => console.log("file changed"));
+
+//review of this and bind
+let person = {
+    word: 'hello',
+    speak: function() {
+        console.log(this.word);		//this reaches down to person scope
+    }
+}
+
+person.speak();		//hello, as speak() has access to sound as is within person scope
+
+let speakFunction = person.speak;    //undefined, as current scope (window) does not have access to speak
+speakFunction();
+
+speakFunction = speakFunction.bind(person);  //sets speakFunction to person scope
+speakFunction();           //prints hello, as speakFunction now bound to person scope
+
+
+//Basic composition
+const composedFunction = (functA, functB) =>
+  x => functA(functB(x));
+
+const triple = x => x * 3;
+const square = x => x * x;
+const tripleSquare = composedFunction(triple, square);
+console.log(tripleSquare(6));
+
+const uppercase = string => string.toUpperCase();
+const intense = string => `${string}!!!!`;
+const loud = composedFunction(uppercase, intense);
+console.log(loud('This sentence is loud'));
+
+const composedFunction2 = (composedFunction, functC) =>
+  (sentence, original, newWord) => composedFunction(functC(sentence, original, newWord));
+
+const replaceWord = (string, original, newWord) => string.replace(original, newWord);
+const loudReplace = composedFunction2(loud, replaceWord);
+console.log(loudReplace('This sentence is loud, so very very loud', /loud/g, 'noisy'));
+
+const composedFunctionBig = (functA, functB, functC) =>
+  (...args) => functC(functB(functA(...args)));
+
+const upperIntenseReplace = composedFunctionBig(replaceWord, uppercase, intense);
+console.log(upperIntenseReplace('This sentence is not quiet, not quiet at all', /quiet/g, 'silent'));
