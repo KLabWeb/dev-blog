16f723bb3db34c6d085fd77dc9d19ef9f6b960d0
smilingstallman <klabweb@tuta.io>
2019-12-04 17:30:14 -0600

    prod build

---
+++ practice-weeks/week-17-react/build/precache-manifest.f6ece89599325d67fc3fa6dfe8a5cd07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98025033186f741b5508a98c18c8f27a",
+    "url": "/week-17-react/index.html"
+  },
+  {
+    "revision": "1b794d773a7212900c3e",
+    "url": "/week-17-react/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1c3549d409d904571537",
+    "url": "/week-17-react/static/js/2.d963d13a.chunk.js"
+  },
+  {
+    "revision": "1b794d773a7212900c3e",
+    "url": "/week-17-react/static/js/main.977628c4.chunk.js"
+  },
+  {
+    "revision": "07264c6995d43e736d40",
+    "url": "/week-17-react/static/js/runtime-main.6d6bc196.js"
+  }
+]);
