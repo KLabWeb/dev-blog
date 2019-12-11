4e28352e2c3262584e3ab82910c518f95c9fbf11
smilingstallman <klabweb@tuta.io>
2019-12-11 15:34:35 -0600

    week 18, day 3 work - react

--- practice-weeks/week-18-react/build/precache-manifest.a9a0dc4e0f3b5640bc03de75e8475554.js
+++ practice-weeks/week-18-react/build/precache-manifest.a9a0dc4e0f3b5640bc03de75e8475554.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4e0d13f2adb8cfd0f6cc403ecceb941",
+    "revision": "33c5f51ee93d4172d41e53bce75eac41",
     "url": "/practice-weeks/week-18-react/index.html"
   },
   {
-    "revision": "6690b16ffbf17f6effc0",
+    "revision": "d713ca64f8b38aefb719",
     "url": "/practice-weeks/week-18-react/static/css/main.0d84065e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/practice-weeks/week-18-react/static/js/2.a0f7fcaa.chunk.js.LICENSE"
   },
   {
-    "revision": "6690b16ffbf17f6effc0",
-    "url": "/practice-weeks/week-18-react/static/js/main.c27090c8.chunk.js"
+    "revision": "d713ca64f8b38aefb719",
+    "url": "/practice-weeks/week-18-react/static/js/main.1cda9a9b.chunk.js"
   },
   {
     "revision": "4a871b314be3ee8faffc",
