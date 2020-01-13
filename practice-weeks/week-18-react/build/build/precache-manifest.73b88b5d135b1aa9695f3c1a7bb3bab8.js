ccd91e91a31d2e6c7dfc2caed2df4899f0ca6b59
smilingstallman <klabweb@tuta.io>
2020-01-13 08:10:25 -0600

    week 20 addition and roadmap updates

---
+++ practice-weeks/week-18-react/build/build/precache-manifest.73b88b5d135b1aa9695f3c1a7bb3bab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aae287c578845d265bc4346f19fd851",
+    "url": "/practice-weeks/week-18-react/index.html"
+  },
+  {
+    "revision": "130cf18c77a6a50603ff",
+    "url": "/practice-weeks/week-18-react/static/css/main.0d84065e.chunk.css"
+  },
+  {
+    "revision": "a86b54c26cc996795834",
+    "url": "/practice-weeks/week-18-react/static/js/2.a0f7fcaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/practice-weeks/week-18-react/static/js/2.a0f7fcaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "130cf18c77a6a50603ff",
+    "url": "/practice-weeks/week-18-react/static/js/main.db0549a5.chunk.js"
+  },
+  {
+    "revision": "4a871b314be3ee8faffc",
+    "url": "/practice-weeks/week-18-react/static/js/runtime-main.ca4defe4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practice-weeks/week-18-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
